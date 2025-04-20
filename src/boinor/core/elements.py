0f89b6867f607eca1c92c53bfd8760ea968dd322
Thorsten Alteholz <boinor@alteholz.dev>
2025-04-20 09:25:37 +0200

    remove ()

--- src/boinor/core/elements.py
+++ src/boinor/core/elements.py
@@ -427,7 +427,7 @@ def rv2coe(k, r, v, tol=1e-8):
             print("XXX  e_ch ", e_ch)
             print("XXX  F ", np.log((e_ch + e_sh) / (e_ch - e_sh)) / 2)
             print("XXX ecc: ", ecc)
-            nu = F_to_nu((np.log((e_ch + e_sh) / (e_ch - e_sh)) / 2), ecc)
+            nu = F_to_nu(np.log((e_ch + e_sh) / (e_ch - e_sh)) / 2, ecc)
 
         raan = np.arctan2(n[1], n[0]) % (2 * np.pi)
         px = r @ n
