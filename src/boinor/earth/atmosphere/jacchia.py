FIX: add tests for Jacchia77 and fix units

src/boinor/earth/atmosphere/jacchia.py
@@ -31,17 +31,19 @@ class Jacchia77:
         # set some values to silence pylint
         self.Z = 0 * u.km
         self.T = 273.15 * u.K
-        self.CN2 = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.CO2 = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.CO = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.CAr = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.CHe = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.CH = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.CM = np.zeros(3) * 1e6 * (u.m) ** -3
-        self.WM = np.zeros(3) * 1e6 * (u.m) ** -3
+        """according to :cite:t:`Ja77`: the unit of these values is 1/cc"""
+        self.CN2 = np.zeros(3) * (u.cm) ** -3
+        self.CO2 = np.zeros(3) * (u.cm) ** -3
+        self.CO = np.zeros(3) * (u.cm) ** -3
+        self.CAr = np.zeros(3) * (u.cm) ** -3
+        self.CHe = np.zeros(3) * (u.cm) ** -3
+        self.CH = np.zeros(3) * (u.cm) ** -3
+        self.CM = np.zeros(3) * (u.cm) ** -3
+        self.WM = np.zeros(3) * (u.cm) ** -3
 
     def _altitude_profile(self, alt):
-        """set altitude profile"""
+        """set altitude profile
+           according to :cite:t:`Ja77`: the unit of these values is 1/cc"""
         Z, T, CN2, CO2, CO, CAr, CHe, CH, CM, WM = _altitude_profile_fast(
             alt, self.Texo.to_value(u.K), self.x, self.y, self.E5M, self.E6P
         )
@@ -59,13 +61,13 @@ class Jacchia77:
         ) = (
             Z * u.km,
             T * u.K,
-            np.array(CN2) * 1e6 * (u.m) ** -3,
-            np.array(CO2) * 1e6 * (u.m) ** -3,
-            np.array(CO) * 1e6 * (u.m) ** -3,
-            np.array(CAr) * 1e6 * (u.m) ** -3,
-            np.array(CHe) * 1e6 * (u.m) ** -3,
-            np.array(CH) * 1e6 * (u.m) ** -3,
-            np.array(CM) * 1e6 * (u.m) ** -3,
+            np.array(CN2) * (u.cm) ** -3,
+            np.array(CO2) * (u.cm) ** -3,
+            np.array(CO) * (u.cm) ** -3,
+            np.array(CAr) * (u.cm) ** -3,
+            np.array(CHe) * (u.cm) ** -3,
+            np.array(CH) * (u.cm) ** -3,
+            np.array(CM) * (u.cm) ** -3,
             np.array(WM) * 1e-3 * (u.kg / u.mol),
         )
         return (

tests/tests_earth/tests_atmosphere/test_jacchia77.py
@@ -492,32 +492,28 @@ def test_values():
             jacchia_CM,
             jacchia_WM,
         ) = Jacchia77(1000 * u.K).altitude_profile(Z * u.km)
-        print("XXX Z", Z, properties)
-        print(
-            "XXX ",
-            CN2,
-            jacchia_CN2,
-            CO2,
-            jacchia_CO2,
-            CO,
-            jacchia_CO,
-            CAr,
-            jacchia_CAr,
-            CHe,
-            jacchia_CHe,
-            CH,
-            jacchia_CH,
-            CM,
-            jacchia_CM,
-            WM,
-            jacchia_WM,
-        )
         assert_quantity_allclose(T, jacchia_T, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CN2, jacchia_CN2, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CO2, jacchia_CO2, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CO, jacchia_CO, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CAr, jacchia_CAr, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CHe, jacchia_CHe, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CH, jacchia_CH, rtol=1e-5)
-        # XXX does not work yet: assert_quantity_allclose(CM, jacchia_CM, rtol=1e-5)
+        # according to https://git.smce.nasa.gov/ccmc-share/modelwebarchive/-/raw/main/Jacchi-Reference-Atmosphere/testj77.for
+        # the computed values need to be converted to match the values from the table
+        compare_CN2=(np.log10(jacchia_CN2.value)+6) * jacchia_CN2.unit
+        assert_quantity_allclose(CN2, compare_CN2, rtol=1e-5)
+
+        compare_CO2=(np.log10(jacchia_CO2.value)+6) * jacchia_CO2.unit
+        assert_quantity_allclose(CO2, compare_CO2, rtol=1e-5)
+
+        compare_CO=(np.log10(jacchia_CO.value)+6) * jacchia_CO.unit
+        assert_quantity_allclose(CO, compare_CO, rtol=1e-5)
+
+        compare_CAr=(np.log10(jacchia_CAr.value)+6) * jacchia_CAr.unit
+        assert_quantity_allclose(CAr, compare_CAr, rtol=1e-5)
+
+        compare_CHe=(np.log10(jacchia_CHe.value)+6) * jacchia_CHe.unit
+        assert_quantity_allclose(CHe, compare_CHe, rtol=1e-5)
+
+        compare_CH=(np.log10(jacchia_CH.value)+6) * jacchia_CH.unit
+        assert_quantity_allclose(CH, compare_CH, rtol=1e-5)
+
+        compare_CM=(np.log10(jacchia_CM.value)+6) * jacchia_CM.unit
+        assert_quantity_allclose(CM, compare_CM, rtol=1e-5)
+
         assert_quantity_allclose(WM, jacchia_WM, rtol=1e-4)