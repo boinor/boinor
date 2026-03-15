2080eca9dfb26c0f2ac6b963c745c258651653ea
Thorsten Alteholz <boinor@alteholz.dev>
2026-03-15 13:00:12 +0100

    FEAT: first steps to calculate a temperature profile for NRLMSIS

--- src/boinor/core/earth_atmosphere/nrlmsis.py
+++ src/boinor/core/earth_atmosphere/nrlmsis.py
@@ -4,10 +4,38 @@ References: :cite:t:`NRLMSIS`:
 
 """
 
-from numba import njit as jit
+from dataclasses import dataclass
+
 import numpy as np
 
+#: Bates profile reference height and joining height
+#: :cite:t:`Bates1959`:
+#: this is only the value without unit
+core_zetaB = 122.5
 
-@jit
-def dummy_function():
-    i=999
+
+@dataclass
+class Bates_temperature_profile:
+    T_ex: float
+    T_B: float
+    sigma: float
+
+
+def t_of_zeta(zeta, tp):
+    """calculate temperature T depending on geopotential height zeta
+    in:  zeta, float	geopotential height
+    in:  tp, Bates termospheric temperate profile (type Bates_temperature_profile)
+
+    out: T(zeta), float
+
+    there are different calculations for zeta >= and < zetab
+    see :cite:t:`NRLMSIS`: page 3
+    """
+    if zeta >= core_zetaB:
+        return tp.T_ex - (tp.T_ex - tp.T_B) * np.exp(-tp.sigma * (zeta - core_zetaB))
+    else:
+        message = "case zeta < zetab not yet implemented"
+        raise NotImplementedError(message)
+
+    message = "how did we arrive here"
+    raise NotImplementedError(message)
--- tests/tests_earth/tests_atmosphere/test_nrlmsis.py
+++ tests/tests_earth/tests_atmosphere/test_nrlmsis.py
@@ -1,13 +1,26 @@
 """tests related to module nrlmsis in sub-package atmosphere of earth"""
-from astropy import units as u
 from astropy.tests.helper import assert_quantity_allclose
-import numpy as np
 import pytest
 
-from boinor.earth.atmosphere.nrlmsis import Nrlmsis
+import boinor.core.earth_atmosphere.nrlmsis as cnrlmsis
 
 # even internal stuff needs to be tested
 # pylint: disable=protected-access
 
-def test_values():
-    print("XXX this is just a dummy test")
+
+def test_t_of_zeta():
+    temperature_profile = cnrlmsis.Bates_temperature_profile(0, 0, 0)
+
+    # XXX this case is still missing
+    with pytest.raises(NotImplementedError, match="case zeta < zetab not yet implemented"):
+        t_of_zeta = cnrlmsis.t_of_zeta(50, temperature_profile)
+
+    T_ex = 12.0
+    temperature_profile_t1 = cnrlmsis.Bates_temperature_profile(T_ex, 0, 0)
+    t_of_zeta = cnrlmsis.t_of_zeta(cnrlmsis.core_zetaB + 120, temperature_profile_t1)
+    assert_quantity_allclose(t_of_zeta, 0.0, rtol=1e-16)
+
+    T_ex = 1000.0
+    temperature_profile_t1 = cnrlmsis.Bates_temperature_profile(T_ex, 0, 0)
+    t_of_zeta = cnrlmsis.t_of_zeta(cnrlmsis.core_zetaB + 120, temperature_profile_t1)
+    assert_quantity_allclose(t_of_zeta, 0.0, rtol=1e-16)
